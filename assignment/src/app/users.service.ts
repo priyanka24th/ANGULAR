c60b5cf4de5e82e5eb458c525471a8515ea1bfd0
priyanka24th <33574259+priyanka24th@users.noreply.github.com>
2021-12-07 11:55:13 +0530

    My_Rec

--- assignment/src/app/users.service.ts
+++ assignment/src/app/users.service.ts
@@ -8,4 +8,14 @@ export class UsersService {
   inactiveUsers = ['Chris', 'Manu'];
 
   constructor() { }
+  setToInactive(id:number){
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+    console.log(this.inactiveUsers)
+  }
+  SetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+    console.log(this.activeUsers)
+  }
 }
